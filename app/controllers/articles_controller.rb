416b6c36ce8f9869d5ed210d4001de861eeb9693
Mradul <mradul.codenote@gmail.com>
2024-09-04 11:41:53 +0530

    new project

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+	def index
+		@article = Article.all
+	end
+
+	def show
+		@article = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(strong)
+		if @article.save
+			redirect_to @article
+		else
+			render :new,status: :undefine
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		binding.pry
+		@article = Article.find(params[:id])
+		if @article.update(strong)
+			redirect_to @article
+		else
+			render :edit,status: :undefine
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+	end
+
+	private
+	def strong
+		binding.pry
+    params.require(:article).permit(:name, :age, :address)
+	end
+end	
